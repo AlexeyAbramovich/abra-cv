96a1eb4a859897d9d3d3a13d7fbf6cf7350dbb92
AlexAbra <alexgithub444356@gmail.com>
2024-09-27 10:01:50 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: '/abra-cv/',
 	resolve: {
 		alias: {
 			$ui: '/src/components/ui/',
