6dcbb864cf469498fc95ba436506caf5b5b3bc9b
AlexAbra <alexgithub444356@gmail.com>
2025-03-23 15:50:55 +0300

    fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import MusicPlayer from '$ui/music-player/MusicPlayer'
 import MusicContextProvider from '$ui/music-player/provider/MusicContextProvider'
 import ShowPlayerButton from '$ui/show-player-button/ShowPlayerButton'
 import { useMemo, useState } from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { useCloseMusicPlayer } from './hooks/useCloseMusicPlayer'
 import { usePreloadFirstCarouselImage } from './hooks/usePreloadFirstCarouselImage'
 import Router from './router/Router'
@@ -17,9 +17,9 @@ function App() {
 
 	const router = useMemo(
 		() => (
-			<BrowserRouter basename={import.meta.env.BASE_URL}>
+			<HashRouter>
 				<Router />
-			</BrowserRouter>
+			</HashRouter>
 		),
 		[]
 	)
